0181b7f742adff10c126f0a585e237ef11f32e1e
Ahmet Kasap <aahmetkasap@gmail.com>
2024-03-24 21:44:41 +0300

    created login and register controllers.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,12 +1,24 @@
+const userModel = require('../models/user.model')
+
 const login = async(req,res) => {
+    const user = await userModel.findOne({email : req.body.email})
+    if (user) return res.status(200).json(user)
+    else return res.status(404).json("password or email incorrect")
     
 }
 
 
 
 const register = async(req,res) => {
-    console.log("register")
+    const userDb = new userModel({
+        name : req.body.name,
+        lastname : req.body.lastname, 
+        email : req.body.email,
+        password : req.body.password
+    })
+    userDb.save()
 }
+
 
 module.exports = {
     login,register
