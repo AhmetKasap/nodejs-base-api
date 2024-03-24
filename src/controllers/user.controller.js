d3e7fcfa05d2b567004b345003cb452c199e2573
Ahmet Kasap <aahmetkasap@gmail.com>
2024-03-24 22:36:52 +0300

    created a response class to handle HTTP status codes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,13 +1,12 @@
 const userModel = require('../models/user.model')
+const Response = require('../utils/Response')
 
 const login = async(req,res) => {
     const user = await userModel.findOne({email : req.body.email})
-    if (user) return res.status(200).json(user)
+    if(user) return new Response(user, "welcome !").ok(res)    
     else return res.status(404).json("password or email incorrect")
     
 }
-
-
 
 const register = async(req,res) => {
     const userDb = new userModel({
