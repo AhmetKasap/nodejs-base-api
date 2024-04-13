6fca0436558e388d3bd79fb0b243058252f0a9c8
Ahmet Kasap <aahmetkasap@gmail.com>
2024-04-13 17:49:20 +0300

    added password hashing with bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,13 @@
 const userModel = require('../models/user.model')
 const APIError = require('../utils/Error')
 const Response = require('../utils/Response')
+const bcrypt = require('bcrypt')
 
 const loginController = async(req,res,next) => {
     const user = await userModel.findOne({email : req.body.email})
     if(!user) throw new APIError('User information is incorrect, please try again', 404)
 
-    if(user && req.body.password === user.password) return new Response(user, "login successfull").ok(res)
+    if(user && await bcrypt.compare(req.body.password, user.password)) return new Response(user, "login successfull").ok(res)
     else throw new APIError('User information is incorrect, please try again', 404)
 }
 
@@ -14,12 +15,14 @@ const registerController = async(req,res) => {
     const auth = await userModel.findOne({email:req.body.email})
     if(auth) throw new APIError('This email is already in use, please try different email', 401)
 
+    const password = await bcrypt.hash(req.body.password,10)
     const userDb = new userModel({
         name : req.body.name,
         lastname : req.body.lastname, 
         email : req.body.email,
-        password : req.body.password
+        password : password
     })
+
     const response = await userDb.save()
     if(response) return new Response(null, 'Registration Successful').created(res)
     else throw new APIError('An error occurred during registration', 500)
